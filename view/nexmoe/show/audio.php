4434d46bac1f5cf73a496bde8bdc41df37a26d23
xinb <netwhilt@outlook.com>
2020-09-14 16:44:38 +0800

    Update audio.php

--- view/nexmoe/show/audio.php
+++ view/nexmoe/show/audio.php
@@ -2,7 +2,16 @@
 <?php view::begin('content');?>
 <div class="mdui-container-fluid">
     <div class="nexmoe-item">
-		<audio class="mdui-center" src="<?php e($item['downloadUrl']);?>" controls autoplay style="width: 100%;"  poster="<?php @e($item['thumb'].'&width=176&height=176');?>"></audio>
+		<audio class="mdui-center" src="<?php e($item['downloadUrl']);?>" controls  style="width: 100%;"  poster="<?php @e($item['thumb'].'&width=176&height=176');?>"></audio>
+//关闭autoplay属性
+	</div>
+	<div class="mdui-textfield">
+	  <label class="mdui-textfield-label">下载地址</label>
+	  <input class="mdui-textfield-input" type="text" value="<?php e($url);?>"/>
+	</div>
+	<div class="mdui-textfield">
+	  <label class="mdui-textfield-label">HTML 引用地址</label>
+	  <input class="mdui-textfield-input" type="text" value="<audio class='mdui-center' src='<?php e($url);?>' controls='' style='width: 100%;' poster='&amp;width=176&amp;height=176'></audio>" />
 	</div>
 </div>
 <a href="<?php e($url);?>" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
