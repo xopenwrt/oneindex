52d3cad7217a605ecb11fca75b878ba87ac89afb
xinb <netwhilt@outlook.com>
2020-09-20 18:48:17 +0800

    Update video5.php
    添加视频HTML链接插入功能

--- view/nexmoe/show/video5.php
+++ view/nexmoe/show/video5.php
@@ -7,6 +7,12 @@
 		  <source src="<?php e($item['downloadUrl']);?>" type="video/mp4">
 		</video>
 	</div>
+	<div class="mdui-textfield">
+	    <label class="mdui-textfield-label">HTML引用地址</label>
+	    <input class="mdui-textfield-input" type="text" value="<video class='mdui-video-fluid mdui-center' preload controls poster='<?php @e($item['thumb']);?>'>
+		  <source src='<?php e($item['downloadUrl']);?>' type='video/mp4'>
+		</video>"/>
+	</div>
 </div>
 <a href="<?php e($url);?>" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 <?php view::end('content');?>
